e45aaee162a3be5139b2c91bafc72cc97bf0feae
Sahir Momin <sahirmomin65@gmail.com>
2025-02-25 21:23:12 -0700

    biometeric and authentication working

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:unplugged/pages/home_page.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Unplugged',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Unplugged'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Unplugged',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final LocalAuthentication _auth = LocalAuthentication();
+
+  bool _isauthenticated = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildUI(),
+      floatingActionButton: _authButton(),
+    );
+  }
+
+  Widget _authButton(){
+    return FloatingActionButton(
+      onPressed: () async {
+      if (!_isauthenticated) {
+        final bool canAuthenticateBiometerics = await _auth.canCheckBiometrics;
+        if (canAuthenticateBiometerics) {
+          try {
+            final bool didAuthenticate = await _auth.authenticate(
+                localizedReason: "Please authenticate to access the apps",
+                options: const AuthenticationOptions(
+                  biometricOnly: false,
+              )
+            
+            );
+            
+            setState(() {
+              _isauthenticated = didAuthenticate;
+            });
+          } catch (e) {
+            print(e);
+          }
+        }
+      }else{
+        setState(() {
+          _isauthenticated = false;
+        });
+      }
+
+      setState(() {
+        _isauthenticated = !_isauthenticated;
+      });
+    },
+      child: Icon(_isauthenticated ? Icons.lock : Icons.lock_open, ),
+    );
+  
+  }
+
+  Widget _buildUI(){
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+        const Text(
+          "App Lock",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          )
+        ),
+
+        if (_isauthenticated) const Text(
+            "Instagram Link",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            )
+        ),
+          if (!_isauthenticated) const Text(
+              "*******",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              )
+          )
+      ],
+      ),
+    );
+
+  }
+}
